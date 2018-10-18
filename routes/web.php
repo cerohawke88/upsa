12ad3f0cd54578dbaba1f233d3afaf3936a5d2f4
cleoputra <acesam222@gmail.com>
2018-10-18 10:50:39 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('index/form', 'testing@form')->name('index.form');
 Route::post('index', 'testing@index')->name('index.create');
 
 Route::get('/print', 'testing@print')->name('pages.print');
+Route::view('/test', 'pages.test')->name('pages.test');
