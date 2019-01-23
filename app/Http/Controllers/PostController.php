8d745ec92635e86bc5b1336a8ff53d539aea51b8
cleoputra <acesam222@gmail.com>
2019-01-23 21:41:09 +0700

    logout

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,11 @@ use App\EmergencyContact;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
     	$personalDetails = PersonalDetails::all();
