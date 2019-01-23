f71829d0fa886597db81add84885437a6b60efe1
Vicky Sultan <vickysultan08@gmail.com>
2019-01-23 16:34:06 +0700

    asdasd

--- resources/views/adminPage/detail.blade.php
+++ resources/views/adminPage/detail.blade.php
@@ -432,7 +432,7 @@
                     <!--CONTACT OF EMERGENCY  -->
                     <div class="box box-default collapsed-box">
                       <div class="box-header with-border">
-                        <h3 class="box-title">CONTACT OF EMERGENCYt</h3>
+                        <h3 class="box-title">CONTACT OF EMERGENCY</h3>
 
                         <div class="box-tools pull-right">
                           <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
